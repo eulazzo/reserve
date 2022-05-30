2d785411670997f596706242ffb733aba4bf3598
Lázaro Vanderson <eulazzo@protonmail.com>
2022-05-30 18:41:32 -0300

    hotel page design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">RESERVE</span>
+        <Link to={"/"}>
+          <span className="logo">RESERVE</span>
+        </Link>
         <div className="navItems">
           <button className="navBar">Register</button>
           <button className="navBar">Login</button>
--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,8 +1,92 @@
- 
-const Hotel = () => {
-  return (
-    <div>Hotel</div>
-  )
-}
+import "./hotel.css";
+import { Footer, Header, MailList, Navbar } from "../../components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-export default Hotel
+const Hotel = () => {
+  const photos = [
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707367.jpg?k=cbacfdeb8404af56a1a94812575d96f6b80f6740fd491d02c6fc3912a16d8757&o=&hp=1",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708745.jpg?k=1aae4678d645c63e0d90cdae8127b15f1e3232d4739bdf387a6578dc3b14bdfd&o=&hp=1",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707776.jpg?k=054bb3e27c9e58d3bb1110349eb5e6e24dacd53fbb0316b9e2519b2bf3c520ae&o=&hp=1",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261708693.jpg?k=ea210b4fa329fe302eab55dd9818c0571afba2abd2225ca3a36457f9afa74e94&o=&hp=1",
+    },
+    {
+      src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707389.jpg?k=52156673f9eb6d5d99d3eed9386491a0465ce6f3b995f005ac71abc192dd5827&o=&hp=1",
+    },
+  ];
+
+  return (
+    <div>
+      <Navbar />
+      <Header type={"list"} />
+
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <button className="bookNow">Reserve or Book Now!</button>
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Elton St 125 New York</span>
+          </div>
+          <span className="hotelDistance">
+            Excellent location - 500m from center
+          </span>
+          <span className="hotelPriceHigtlight">
+            Book a stay over $114 at this property and get a free airport taxi.
+          </span>
+
+          <div className="hotelImages">
+            {photos.map((img, index) => (
+              <div key={index} className="hotelImgWrapper">
+                <img src={img.src} alt="" className="hotelImg" />
+              </div>
+            ))}
+          </div>
+
+          <div className="hotelDetails">
+            <div className="hotelDetailsTexts">
+              <h1 className="hotelTitle">Stay in the heart of Kravow</h1>
+              <p className="hotelDesc">
+                Citadines Trafalgar Square London offers modern self-catering
+                accommodation in central London. Trafalgar Square is just 150
+                meters from the property and Embankment Underground Station is a
+                2-minute walk away. The cozy apartments and studios feature
+                satellite TV and a modern sound system, as well as a seating
+                area, a private bathroom and a desk. Citadines apartments also
+                have a kitchenette with a microwave, fridge and tea and coffee
+                making facilities.
+              </p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Perfect for a 9-night stay!</h1>
+              <span>
+                Located in the real heart of Krakow, this property has an
+                excellent location score of 9.8!
+              </span>
+              <h2>
+                <b>$945</b> (9 nights)
+              </h2>
+              <button>Reserve or Book Now!</button>
+            </div>
+          </div>
+        </div>
+
+        <MailList />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Hotel;
