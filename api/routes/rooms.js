ff703730e942a63b5cd2bda3ab89317ac61bd9c4
Lázaro Vanderson <eulazzo@protonmail.com>
2022-06-01 17:09:04 -0300

    node mongodb model relations

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,29 @@
 import express from "express";
+
+import {
+  createRoom,
+  deleteRoom,
+  getAllRooms,
+  getRoom,
+  updateRoom,
+} from "../controllers/room.controller.js";
+
+import { verifyAdmin } from "../utils/VerifyToken.js";
 const router = express.Router();
- 
+
+//  CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//  UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+//  DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//  GET ALL ROOMS
+router.get("/", getAllRooms);
+
+// GET Room
+router.get("/:id", getRoom);
 
 export default router;
